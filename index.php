1b330b31636f67aefcc3e3518425643ad2d9be23
Ivan Blažević <ivan.blazevic97@gmail.com>
2020-09-24 14:10:50 +0200

    twig/php and folder setup

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'vendor/autoload.php';
+
+$loader = new \Twig\Loader\FilesystemLoader('./views');
+$twig = new \Twig\Environment($loader);
+
+echo $twig->render("index.html.twig", array(
+    'name' => 'Ivan'
+));
